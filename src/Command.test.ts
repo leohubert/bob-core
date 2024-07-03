d29ea975b2e2763eeec21a7e8d9f3c092280cc3a
Léo Hubert <leo.hubert@bodyguard.ai>
2024-07-03 15:46:38 +0200

    chore: add more tests

--- src/Command.test.ts
+++ src/Command.test.ts
@@ -1,10 +1,24 @@
 import { Command } from './Command';
+import {MissingRequiredArgumentValue} from "./errors/MissingRequiredArgumentValue";
 
 class MockCommand extends Command {
     signature = 'mockCommand {argument} {--option}';
     description = 'This is a mock command for testing';
 
     protected handle() {
+        const opts = this.option('option');
+        const arg = this.argument('argument');
+
+        if (opts) {
+            return Promise.resolve(11);
+        }
+
+        if (arg === 'value') {
+            return Promise.resolve(1);
+        } else if (arg) {
+            return Promise.resolve(-1);
+        }
+
         return Promise.resolve(0);
     }
 }
@@ -16,6 +30,10 @@ describe('Command', () => {
         command = new MockCommand();
     });
 
+    it('should have a command', () => {
+        expect(command.command).toBe('mockCommand');
+    });
+
     it('should have a signature', () => {
         expect(command.signature).toBe('mockCommand {argument} {--option}');
     });
@@ -25,14 +43,23 @@ describe('Command', () => {
     });
 
 
-    it('should handle command', async () => {
-        const result = await command.run(undefined, 'value', '--option')
-        expect(result).toBe(0);
+    it('should handle command with argument', async () => {
+        const result = await command.run(undefined, 'value')
+        expect(result).toBe(1);
     });
 
-    // Add more tests for other methods in the Command class
-    // For example, if you have a method named `mockMethod` that should return `mockValue`, you can test it like this:
-    // it('should return correct value from mockMethod', () => {
-    //   expect(command.mockMethod()).toBe(mockValue);
-    // });
+    it('should handle command with argument', async () => {
+        const result = await command.run(undefined, 'badValue')
+        expect(result).toBe(-1);
+    });
+
+    it('should throw error if argument is missing', async () => {
+        await expect(command.run(undefined)).rejects.toThrowError(MissingRequiredArgumentValue);
+
+    });
+
+    it('should handle command with option', async () => {
+        const result = await command.run(undefined,  'value', '--option')
+        expect(result).toBe(11);
+    })
 });
